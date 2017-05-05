35366b863a6314849a7cb8626191629c7dd7c116
brianhodges <hodgesbn@yahoo.com>
2017-05-05 02:34:14 +0000

    initial code commit

---
+++ country.py
@@ -0,0 +1,16 @@
+class Country:
+	
+    def __init__(self, arg1, arg2, arg3, arg4):
+        self.name = arg1
+        self.population = arg2
+        self.states = arg3
+        self.best_city = arg4
+        
+    def fav_cnt(self):
+		return len(self.states)
+
+class City:
+	
+	def __init__(self, arg1, arg2):
+		self.name = arg1
+		self.population = arg2
---
+++ main.py
@@ -0,0 +1,30 @@
+from country import Country
+import sys
+from country import City
+
+x = 0
+
+a1 = City('Los Angeles', '4 million')
+b1 = ['New York', 'Virginia', 'California', 'Florida', 'Hawaii']
+c1 = Country('USA','350 million', b1, a1)
+
+a2 = City('Cancun', '700,000')
+b2 = ['Durango', 'Puebla', 'Guerrero', 'Quintana Roo']
+c2 = Country('Mexico', '127 million', b2, a2)
+
+countries = [c1, c2]
+
+sys.stdout = open('log.txt', 'w')
+
+print
+while x < len(countries):
+    print 'Name: ' + countries[x].name
+    print 'Population: ' + countries[x].population
+    print 'Best States: ' + ', '.join(countries[x].states)
+    print 'Top State Count: ' + str(countries[x].fav_cnt())
+    print 'Best City: ' + countries[x].best_city.name
+    print countries[x].best_city.name + ' Population: ' + countries[x].best_city.population
+    print
+    x += 1
+
+sys.stdout.close()
